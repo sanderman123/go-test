37c6dc174170f4f8f69295f6a83825e485bb6742
Sander Valstar <svalstar@bol.com>
2016-09-24 23:13:56 +0200

    Adds login

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"github.com/emicklei/go-restful"
 	"fmt"
 	"net/http"
 	"log"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"golang.org/x/crypto/bcrypt"
+	"github.com/emicklei/go-restful"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
 var collection = &mgo.Collection{}
@@ -41,6 +43,7 @@ func New() *restful.WebService {
 	service.Route(service.POST("").To(CreateUser))
 	service.Route(service.PUT("").To(UpdateUser))
 	service.Route(service.DELETE("/{user-name}").To(DeleteUser))
+	service.Route(service.POST("/login").To(AuthenticateUser))
 
 	return service
 }
@@ -153,6 +156,49 @@ func DeleteUser(request *restful.Request, response *restful.Response) {
 		response.WriteHeader(http.StatusOK)
 	} else if err == mgo.ErrNotFound {
 		response.WriteHeader(http.StatusNotFound)
+	} else {
+		response.WriteError(http.StatusInternalServerError, err)
+	}
+}
+
+func AuthenticateUser(request *restful.Request, response *restful.Response) {
+	usr := new(User)
+	err := request.ReadEntity(&usr)
+	if err != nil {
+		response.WriteError(http.StatusInternalServerError, err)
+		return
+	}
+
+	unHashedPassword := usr.Password
+	err = collection.Find(bson.M{"username": usr.UserName}).One(&usr)
+	if err != nil {
+		response.WriteError(http.StatusInternalServerError, err)
+		return
+	}
+
+	// Comparing the password with the hash
+	err = bcrypt.CompareHashAndPassword([]byte(usr.Password), []byte(unHashedPassword))
+	if err != nil {
+		log.Println("Error comparing password to hash ", err)
+		response.WriteError(http.StatusUnauthorized, err)
+	}
+
+	usr.Password = ""
+
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userName": usr.UserName,
+		"nbf": time.Date(2016, 9, 24, 12, 0, 0, 0, time.UTC).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte("my-secret"))
+
+	fmt.Println(tokenString, err)
+
+	if err == nil {
+		response.WriteEntity(tokenString)
 	} else {
 		response.WriteError(http.StatusInternalServerError, err)
 	}
